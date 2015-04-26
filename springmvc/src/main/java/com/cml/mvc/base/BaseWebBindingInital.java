9e4d1758c305fbf32d0029d8a195ca1d88a410a9
menglin <865517964@qq.com>
2015-04-26 16:04:47 +0800

    update

--- springmvc/src/main/java/com/cml/mvc/base/BaseWebBindingInital.java
+++ springmvc/src/main/java/com/cml/mvc/base/BaseWebBindingInital.java
@@ -4,12 +4,14 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.joda.time.DateTime;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.validation.Validator;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.support.ConfigurableWebBindingInitializer;
 import org.springframework.web.bind.support.WebBindingInitializer;
 import org.springframework.web.context.request.WebRequest;
 
 import com.cml.mvc.property.editor.JodaTimePropertyEditor;
+import com.cml.mvc.validator.UserValidator;
 
 public class BaseWebBindingInital extends ConfigurableWebBindingInitializer {
 	private static final Log LOG = LogFactory
@@ -20,6 +22,7 @@ public class BaseWebBindingInital extends ConfigurableWebBindingInitializer {
 	@Override
 	public void initBinder(WebDataBinder binder, WebRequest request) {
 
+		LOG.info("validators===>"+binder.getValidators());
 		super.initBinder(binder, request);
 		// 没有找到propertyEditor，会自动使用属性转换数据
 		// binder.registerCustomEditor(DateTime.class, new
--- springmvc/src/main/java/com/cml/mvc/controller/ValidTestController.java
+++ springmvc/src/main/java/com/cml/mvc/controller/ValidTestController.java
@@ -7,14 +7,24 @@ import org.apache.commons.logging.LogFactory;
 import org.joda.time.DateTime;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.cml.mvc.model.User;
+import com.cml.mvc.validator.UserValidator;
 
 @Controller
 public class ValidTestController {
 
 	Log log = LogFactory.getLog(ValidTestController.class);
+
+	@InitBinder
+	public void init(WebDataBinder binder) {
+		log.debug("=======init==============");
+		binder.addValidators(new UserValidator());
+	}
 
 	@RequestMapping("/user")
 	public String testUser(@Valid @ModelAttribute("user") User user,
