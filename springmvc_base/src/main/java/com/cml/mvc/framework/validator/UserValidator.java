b47f6e8d7021a3922938fb691f252253a8e8a682
menglin.chen <teamlab@192.168.1.131>
2015-04-27 22:21:57 +0800

    update

--- springmvc_base/src/main/java/com/cml/mvc/framework/validator/UserValidator.java
+++ springmvc_base/src/main/java/com/cml/mvc/framework/validator/UserValidator.java
@@ -3,7 +3,6 @@ package com.cml.mvc.framework.validator;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.validation.Errors;
-import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import com.cml.mvc.framework.model.User;
