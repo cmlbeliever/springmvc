a898662d84f6f6179edf7674f42ee8bac0469df8
menglin.chen <teamlab@192.168.1.131>
2015-04-30 15:51:38 +0800

    设置jodatime返回日期格式

--- springmvc_base/src/main/java/com/cml/mvc/framework/converter/DateTimeConverter.java
+++ springmvc_base/src/main/java/com/cml/mvc/framework/converter/DateTimeConverter.java
@@ -1,21 +1,8 @@
 package com.cml.mvc.framework.converter;
 
-import java.io.IOException;
-
-import org.joda.time.DateTime;
-
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.JsonSerializer;
+import com.cml.mvc.framework.converter.datetime.BaseJodaModule;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationConfig;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.cfg.SerializerFactoryConfig;
-import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
-import com.fasterxml.jackson.databind.ser.BasicSerializerFactory;
-import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
-import com.fasterxml.jackson.databind.ser.SerializerFactory;
-import com.fasterxml.jackson.databind.ser.Serializers;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 /**
  * DateTime json返回日期格式化
@@ -26,8 +13,10 @@ import com.fasterxml.jackson.databind.ser.Serializers;
  */
 public class DateTimeConverter extends ObjectMapper {
 
-	public static final String DATE_FORMATTER = "yyyyMMddHHmmss";
+	private static final long serialVersionUID = 1L;
 
 	public DateTimeConverter() {
+		this.registerModule(new BaseJodaModule());
+		this.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 	}
 }
